5af5bba4683b813220f32672c4e550a5719cd4ae
Michael Trew <40902786+mtrew2015@users.noreply.github.com>
2020-02-05 06:32:04 -0500

    formats
    formats app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
 import "./styles.css";
-import {useHistory} from 'react-router-dom';
-import PrivateRoute from './PrivateRoute';
+import { useHistory } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 import Test from "./Test";
 
 export default function App() {
-  const history = useHistory()
+  const history = useHistory();
   const signInHandler = () => {
-    localStorage.setItem("token", 'token');
-    history.push('/test')
-  }
+    localStorage.setItem("token", "token");
+    history.push("/test");
+  };
   return (
     <div className="App">
       <button onClick={() => signInHandler()}>Sign In </button>
       <button onClick={() => localStorage.clear()}>Delete Token</button>
-      <PrivateRoute path="/test" component={Test} hello="helloWorld" goodBye="GoodBye"/>
-      
-      
+      <PrivateRoute
+        path="/test"
+        component={Test}
+        hello="helloWorld"
+        goodBye="GoodBye"
+      />
     </div>
   );
 }
