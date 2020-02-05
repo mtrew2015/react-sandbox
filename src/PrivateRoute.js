ce970fcdd9e5842ed081ce323a50a32c9fd18741
Michael Trew <40902786+mtrew2015@users.noreply.github.com>
2020-02-05 06:33:28 -0500

    formats
    formats a bit

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-const PrivateRoute = ({ component: Component, path, ...props}) => {
-  
-  if(localStorage.getItem("token")){
+const PrivateRoute = ({ component: Component, path, ...props }) => {
+  if (localStorage.getItem("token")) {
     return (
       <Route path={path}>
         <Component {...props} />
       </Route>
     );
   } else {
-    return <Redirect to="/"/>
+    return <Redirect to="/" />;
   }
- 
 };
 export default PrivateRoute;
--- src/Test.js
+++ src/Test.js
@@ -1,8 +1,7 @@
 import React from "react";
 
-
 export default function Test(props) {
-  console.log(props)
+  console.log(props);
   return (
     <div className="App">
       <h1>Hello Test</h1>
